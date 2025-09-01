808253518017f7698e9ee4ccb3e61037aed249f8
Bleyle823 <bennybleylle09@gmail.com>
2025-09-01 20:17:36 +0300

    Enhance layout of App component by centering SquidWidgetComponent within the main section for improved visual alignment and user experience.

--- squidRouter/app/page.tsx
+++ squidRouter/app/page.tsx
@@ -5,7 +5,7 @@ import { SquidWidgetComponent } from "./components/DemoComponents";
 export default function App() {
   return (
     <div className="flex flex-col min-h-screen font-sans">
-      <main className="flex-1 p-4">
+      <main className="flex-1 flex items-center justify-center p-4">
         <SquidWidgetComponent />
       </main>
     </div>
